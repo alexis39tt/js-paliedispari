ed0db230228bcfe345c40d58c7f306a95974115c
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-19 18:22:38 +0100

    exercise 2 completed

--- pariedispari/assets/scripts/script.js
+++ pariedispari/assets/scripts/script.js
@@ -4,26 +4,58 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-function Random(num, type){
-    let cpu = Math.ceil(Math.random()*5);
-    console.log(num);
-    console.log(cpu);
-    if(type == "even"){
-        console.log("even")
+function RandomNumber(){
+    let x = Math.ceil(Math.random()*5);
+    container[0].innerHTML = `<p>Il numero del CPU è ${x}</p>`;
+    return x;
+}
+
+function UserNum(){
+    let y = Number(document.getElementById("num").value);
+    container[1].innerHTML = `<p>Il tuo numero è ${y}</p>`;
+    return y;
+}
+
+function Sum(num, rnum){
+    let z = Number(num)+Number(rnum);
+    container[2].innerHTML = `<p>La somma dei numeri è ${z}</p>`
+    return z;
+}
+
+function Buttons(type, sum){
+    if (sum % 2 == 0){
+        if(type == "pari"){
+            container[3].innerHTML = `<span class="v">La somma è pari. Hai vinto!</span>`
+        }
+        else{
+            container[3].innerHTML = `<span class="p">La somma è dispari. Hai perso!</span>`
+        }
     }
     else{
-        console.log("odd")
+        if(type == "dispari"){
+            container[3].innerHTML = `<span class="v">La somma è dispari. Hai vinto!</span>`
+        }
+        else{
+            container[3].innerHTML = `<span class="p">La somma è pari. Hai perso!</span>`
+        }
     }
 }
 
-let num = Number(document.getElementById("num").value);
-let even = document.getElementById("even");
-let odd = document.getElementById("odd");
+const container = [document.getElementById("container1"), document.getElementById("container2"), document.getElementById("container3"), document.getElementById("container4")];
+let type;
+let pari = document.getElementById("even");
+let dispari = document.getElementById("odd");
 
-even.addEventListener("click", () => {
-    Random(num, "even");
+pari.addEventListener("click", () => {
+    let num = UserNum();
+    let rnum = RandomNumber();
+    let sum = Sum(num, rnum);
+    Buttons("pari", sum);
 })
 
-odd.addEventListener("click", () => {
-    Random(num, "odd");
+dispari.addEventListener("click", () => {
+    let num = UserNum();
+    let rnum = RandomNumber();
+    let sum = Sum(num, rnum);
+    Buttons("dispari", sum);
 })
